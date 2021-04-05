e6254f29c461dfe463ca2c24f7a000bfde731e77
ahmadmurteza <59716267+ahmadmurteza@users.noreply.github.com>
2021-04-05 17:46:25 +0800

    Memanggil method dari banyak class di package lain

---
+++ model/Dosen.java
@@ -0,0 +1,12 @@
+package model;
+
+
+public class Dosen{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("=== Data Dosen =========");
+  System.out.println("NIDN : 1103022101");
+  System.out.println("Nama : Ahmad Murteza Akbari");
+  System.out.println("=====================================");
+ }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,12 @@
+package model;
+
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("=== Data Mata Kuliah =========");
+  System.out.println("Kode : TIK33021");
+  System.out.println("Nama : Ahmad Murteza Akbari");
+  System.out.println("=====================================");
+ }
+}
